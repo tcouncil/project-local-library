84c04d97c6dd5e1db3a0c1d6ef910fd1294cf5c8
Travis Council <travis.council315@gmail.com>
2020-12-15 15:58:20 -0700

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,23 @@
-function findAuthorById(authors, id) {}
+const findAuthorById = (authors, id) => authors.find(e => e.id === id);
 
-function findBookById(books, id) {}
+const findBookById = (books, id) => books.find(e => e.id === id);
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const borrowedBooks = books.filter(book => !book.borrows[0].returned);
+  const returnedBooks = books.filter(book => book.borrows[0].returned);
 
-function getBorrowersForBook(book, accounts) {}
+  return [borrowedBooks, returnedBooks];
+}
+
+function getBorrowersForBook(book, accounts) {
+  let returnArr = [];
+  for(let key in book.borrows){
+    const accountObj = accounts.find(element => element.id === book.borrows[key].id );
+    accountObj.returned = book.borrows[key].returned;
+    returnArr.push(accountObj);
+  }
+  return returnArr.slice(0,10);
+}
 
 module.exports = {
   findAuthorById,
