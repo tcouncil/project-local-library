57ea9916409861feee7169e8b4322af7cead16fa
Travis Council <travis.council315@gmail.com>
2020-12-15 15:56:52 -0700

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,26 @@
-function findAccountById(accounts, id) {}
+const findAccountById = (accounts, id) => accounts.find(element => element.id === id);
 
-function sortAccountsByLastName(accounts) {}
+const sortAccountsByLastName = (accounts) => accounts.sort((wordA, wordB) => wordA.name.last.toLowerCase() > wordB.name.last.toLowerCase() ? 1 : -1);
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  let num = 0;
+  for (let key in books) {
+    num += books[k].borrows.filter(e => e.id === account.id).length;
+  }
+  return num;
+}
 
-function booksInPossession(account, books, authors) {}
+function booksInPossession(account, books, authors) {
+  const returnArr = [];
+  for (let key in books) {
+    if(books[key].borrows.some(element => element.id === account.id && !element.returned)){
+      const bookObject = books[key];
+      bookObject.author = authors.find(element => element.id === bookObject.authorId);
+      returnArr.push(bookObject);
+    }
+  }
+  return returnArr;
+}
 
 module.exports = {
   findAccountById,
